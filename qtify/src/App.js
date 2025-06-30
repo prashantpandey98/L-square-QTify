5d590776d14824f4164ef70da5419d1c48d916ce
Prashant <prashant.pandey@app-scoop.com>
2025-06-30 13:57:23 +0530

    qtify app setup

---
+++ qtify/src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import Hero from "./components/Hero/Hero";
+import HomePage from "./pages/HomePage/HomePage";
+import Navbar from "./components/Navbar/Navbar";
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
+import { Outlet } from "react-router-dom";
+import {
+  fetchFilters,
+  fetchNewAlbums,
+  fetchSongs,
+  fetchTopAlbums,
+} from "./api/api";
+
+function App() {
+  const [data, setData] = useState({});
+
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevState) => {
+        return { ...prevState, [key]: data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+    generateData("genres", fetchFilters);
+  }, []);
+
+  const { topAlbums = [], newAlbums = [], songs = [], genres = [] } = data;
+
+  return (
+    <>
+      <StyledEngineProvider injectFirst>
+        <Navbar searchData={[...topAlbums, ...newAlbums]} />
+        <Outlet context={{ data: { topAlbums, newAlbums, songs, genres } }} />
+      </StyledEngineProvider>
+    </>
+  );
+}
+
+export default App;
